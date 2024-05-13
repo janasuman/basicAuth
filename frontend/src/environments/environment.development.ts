34ab71cd5e8e2e4318bdd0932d416042f0bde1a4
janasuman <suman121jana@gmail.com>
2024-05-13 23:24:03 +0530

    setup frontend

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://example.com/api' // Replace this with your actual production API URL
+  };
+  
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api' // Replace this with your actual development API URL
+  };
+  
